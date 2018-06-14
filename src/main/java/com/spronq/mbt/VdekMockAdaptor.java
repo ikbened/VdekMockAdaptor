534a18fbddc639cefc9d4ad309ea207209205533
ikbened <ed.hilkman@spronq.com>
2018-06-14 21:35:25 +0200

    Added new api call GetUserById

--- src/main/java/com/spronq/mbt/VdekMockAdaptor.java
+++ src/main/java/com/spronq/mbt/VdekMockAdaptor.java
@@ -4,21 +4,29 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 /**
- * Hello world!
+ * For testing use netcat (nc -v 127.0.0.1 4444)
  *
  */
 public class VdekMockAdaptor {
+    private static final String baseUrl = "http://localhost:8080";
 
     public static void main(String[] args) throws IOException {
         int port = 4444;
@@ -32,12 +40,13 @@ public class VdekMockAdaptor {
             In in = new In(clientSocket);
             Out out = new Out(clientSocket);
 
-            String s;
-            String ss;
-            while ((s = in.readLine()) != null) {
-                System.out.println(s);
-                ss = processLine(s);
-                System.out.println("ss:" + ss);
+            String lineIn;
+            String lineOut;
+            while ((lineIn = in.readLine()) != null) {
+                System.out.println("in:  " + lineIn);
+                lineOut = callApi(lineIn);
+                out.println(lineOut);
+                System.out.println("out: " + lineOut);
             }
 
             System.err.println("Closing connection with client: " + clientSocket.getInetAddress());
@@ -47,34 +56,129 @@ public class VdekMockAdaptor {
         }
     }
 
-    private static String processLine(String line) {
+    private static String callApi(String line) {
+        String response = "";
         String[] tokens = line.split("[(,)]+");
-        System.out.println(java.util.Arrays.toString(tokens));
 
         switch(tokens[0]) {
-            case "GetUserByEmail_Req":
+            case "PostUser_Req":
+                break;
+            case "GetUsersByEmail_Req":
+                response = GetUsersByEmail(tokens);
                 break;
             case "GetUserById_Req":
+                response = GetUserById(tokens);
+                break;
+            case "PostShipment_Req":
                 break;
             case "GetShipment_Req":
-                getShipmentReq(tokens);
+                response = getShipment(tokens);
                 break;
             default :
-                getShipmentReq(tokens); //Just for testing purposes only....
                 System.out.println("ERR - Unknown call: " + tokens[0]);
         }
 
-
-        return "";
+        return response;
     }
 
-    private static void getShipmentReq(String[] tokens) {
-        String url = "http://localhost:8080/shipments/1";
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders httpHeaders = restTemplate.headForHeaders(url);
+    private static String GetUserById(String[] tokens) {
+        RestTemplate restTemplate;
+        String response;
 
-        ResponseEntity response = restTemplate.getForEntity(url, String.class);
-        System.out.println(response);
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(baseUrl + "/users/" + tokens[1]);
+        String uriBuilder = builder.build().encode().toUriString();
+
+        try {
+            restTemplate = new RestTemplate();
+            String re = restTemplate.getForObject(uriBuilder, String.class);
+            response = makeUserOutput(new JSONObject(re));
+            response = "GetUser_Resp(" + response + ")";
+        } catch (HttpClientErrorException ex) {
+            response = "GetUser_Resp_Err(" + ex.getRawStatusCode() + ")";
+        }
+
+        return response;
+    }
+
+
+    private static String GetUsersByEmail(String[] tokens) {
+        RestTemplate restTemplate;
+        String response = "";
+
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(baseUrl + "/users")
+                .queryParam("email", tokens[1]);
+        String uriBuilder = builder.build().encode().toUriString();
+
+        try {
+            restTemplate = new RestTemplate();
+            String re = restTemplate.getForObject(uriBuilder, String.class);
+
+            JSONArray users = new JSONArray(re);
+            for (int i = 0; i < users.length(); i++) {
+                if (!StringUtils.isEmpty(response)) {
+                    response = response + ",";
+                }
+                response = response + makeUserOutput(users.getJSONObject(i));
+                System.out.println(response);
+            }
+            response = "GetUser_Resp(" + response + ")";
+        } catch (HttpClientErrorException ex) {
+            response = "GetUser_Resp_Err(" + ex.getRawStatusCode() + ")";
+        }
+
+        return response;
+    }
+
+
+    private static String getShipment(String[] tokens) {
+        String url = baseUrl + "/shipments/" + tokens[1];
+        RestTemplate restTemplate;
+        String response;
+
+        try {
+            restTemplate = new RestTemplate();
+            ResponseEntity re = restTemplate.getForEntity(url, String.class);
+            response = makeShipmentOutput(new JSONObject(re.getBody().toString()));
+            response = "GetShipment_Resp(" + response + ")";
+        } catch (HttpClientErrorException ex) {
+            response = "GetShipment_Resp_Err(" + ex.getRawStatusCode() + ")";
+        }
+
+        return response;
+    }
+
+    private static String makeShipmentOutput(JSONObject shipment) {
+        List<String> attributes =  Arrays.asList("shipmentId","emailAddress","emailUser","processedByTask","errorMessage");
+        String response = "";
+
+        for(String attribute:attributes){
+            if(!StringUtils.isEmpty(response)) {
+                response = response + ",";
+            }
+
+            if (shipment.has(attribute)) {
+                response = response + shipment.get(attribute);
+            }
+        }
+        return response;
+    }
+
+    private static String makeUserOutput(JSONObject user) {
+        String response = "";
+
+        List<String> attributes =  Arrays.asList("id", "label", "email", "customerNumber", "accountSetId", "postalCode");
+        for(String attribute:attributes){
+            if(!StringUtils.isEmpty(response)) {
+                response = response + ",";
+            }
+
+            if (user.has(attribute)) {
+                response = response + user.get(attribute);
+            }
+        }
+        return "User(" + response + ")";
     }
 }
